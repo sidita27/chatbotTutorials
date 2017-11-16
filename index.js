6eed6ab9b4cf7b5a541a71992850f3dd2542f7b3
Sidita Duli <siditaduli27@gmail.com>
2017-11-16 21:46:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.set('port',(process.env.PORT || 5000))
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
+let token="EAAB2YJ4SwesBALdfTWIK56n2BtHkprSgVgZC4Ry4oZATq1pMuZBbUPxJLxerZCLAY9axjBUY9NcYCZAZC4J4unCZBNSlPxtaHE6E7oiP8ufScoGrmiYqrGZAiz8hQIAJ5S7EQEmRqp9pO6MxjrdmWGMIZC61oEoFxMbLEopBvhaiPMgZDZD"
+
 app.get('/', function (req,res){
 	res.send("Jam nje robot")
 })
@@ -19,6 +21,38 @@ app.get('/webhook/',function(req,res){
 	}
 	res.send("wrong token")
 })
+app.post('/webhook/',function(req,res){
+	let messaging_events=req.body.entry[0].messaging
+	for(let i=0;i<messaging_events.length;i++){
+		let event=messaging_events[i]
+		let sender=event.sender.id
+		if(event.message&&event.message.text){
+		let text=event.message.text
+		sendtext(sender,"Shm mire e the,  "+text.substring(0,100))
+		}
+	}
+	res.sendStatus(200)
+})
+
+function sendText(sender,text){
+	let messageData={text:text}
+	request({
+		url:"https://graph.facebook.com/v2.6/me/messages/",
+		qs:{access_token:token}
+		method:"POST"
+		json:{
+			recipient:{id:sender},
+			message:messageData
+		}
+	},function(error,response,body){
+		if(error){
+			console.log("sending error")
+		}else if(response.body.error){
+			console.log("response body error")
+		}
+	})
+}
+
 app.listen(app.get('port'),function(){
 	console.log("running:port")
 })
